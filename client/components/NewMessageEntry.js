35579d40fb686884b0a8390f5e57a926d718b3ba
Michael Douglas <michaelseandouglas@gmail.com>
2017-10-12 11:51:18 -0400

    working chat functionality

--- client/components/NewMessageEntry.js
+++ client/components/NewMessageEntry.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import store, { createNewMessage,receivedNewMessage } from '../store';
+import axios from 'axios'
+import socket from '../socket'
 
 export default class NewMessageEntry extends Component {
+  constructor (props) {
+    super(props);
+    this.state = store.getState();
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount () {
+    this.unsubscribeFromStore = store.subscribe(() => {
+      this.setState(store.getState())
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromStore();
+  }
+
+  handleChange(event){
+    store.dispatch(createNewMessage(event.target.value))
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    // our message content is on our state, which we're getting from our Redux store
+    const content = this.state.newMessageEntry
+    // our channelId is available from the props sent by MessagesList, which it receives as props from the Route!
+    const channelId = this.props.channelId
+
+    axios.post('/api/messages/', {
+      content : content,
+      channelId : channelId
+    })
+    .then(res => res.data)
+    .then(message => {
+      store.dispatch(receivedNewMessage(message));
+      socket.emit('new-message', message)
+    })
+
+  }
 
   render () {
     return (
-      <form id="new-message-form">
+      <form id="new-message-form" onSubmit={this.handleSubmit}>
         <div className="input-group input-group-lg">
           <input
             className="form-control"
             type="text"
             name="content"
+            value={this.state.newMessageEntry}
+            onChange={this.handleChange}
             placeholder="Say something nice..."
           />
           <span className="input-group-btn">
--- client/store.js
+++ client/store.js
@@ -2,6 +2,8 @@ import { createStore } from 'redux';
 
 // ACTION TYPES
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+const CREATE_NEW_MESSAGE = "CREATE_NEW_MESSAGE"
+const RECEIVED_NEW_MESSAGE = "RECEIVED_NEW_MESSAGE"
 
 // ACTION CREATORS
 export function gotMessagesFromServer(messages) {
@@ -11,9 +13,25 @@ export function gotMessagesFromServer(messages) {
   };
 }
 
+export function createNewMessage(message){
+  return {
+    type: CREATE_NEW_MESSAGE,
+    newMessageEntry: message
+  }
+
+}
+
+export function receivedNewMessage(message){
+  return{
+    type: RECEIVED_NEW_MESSAGE,
+    message
+  }
+}
+
 // INITIAL STATE
 const initialState = {
-  messages: []
+  messages: [],
+  newMessageEntry: ''
 };
 
 // REDUCER
@@ -21,6 +39,10 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case GOT_MESSAGES_FROM_SERVER:
        return Object.assign({}, state, { messages: action.messages });
+    case CREATE_NEW_MESSAGE:
+       return Object.assign({}, state, {newMessageEntry : action.newMessageEntry})
+    case RECEIVED_NEW_MESSAGE:
+      return Object.assign({}, state, { messages: state.messages.concat(action.message) });
     default:
        return state;
   }
@@ -29,3 +51,18 @@ function reducer(state = initialState, action) {
 // STORE
 const store = createStore(reducer);
 export default store;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
